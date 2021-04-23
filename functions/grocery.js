695d93cf5a3578c8b673f2751ccbf7807f0e5676
Ishan Manandhar <ishan02016@gmail.com>
2021-04-23 18:30:52 +0545

    added grocery listing with fun texts

--- functions/grocery.js
+++ functions/grocery.js
@@ -1,6 +1,17 @@
 exports.handler = async function (event, _context) {
+  funLines = [
+    ' this is a decilious item!',
+    ' must be eaten fresh!',
+    ' healthy choice!',
+    ' fresh green veggies huh!',
+  ];
+
+  Array.prototype.randomize = function () {
+    return this[Math.floor(Math.random() * this.length)];
+  };
+
   return {
     statusCode: 200,
-    body: `${event.queryStringParameters.input}`,
+    body: `${event.queryStringParameters.input}` + funLines.randomize(),
   };
 };
--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,7 @@
 exports.handler = async function (event, context) {
+
   return {
     statusCode: 200,
-    body: JSON.stringify({ message: 'Welcome to my serverless grocery store.' }),
+    body: JSON.stringify({ message: 'Welcome to my serverless grocery store.'}),
   };
 };
